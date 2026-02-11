6a033883fa75ed07498b757e1bfebaf6b21006f4
nedmad <matheusmned@gmail.com>
2026-02-11 12:40:13 -0300

    Efeito de digitação no component inro

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,19 +1,10 @@
+"use client"
+import { useEffect, useState } from "react";
 import Container from "../container/container";
-
+import { motion } from "motion/react";
 export default function Intro() {
-  return (
-    <section className="h-screen snap-start bg-blue-900">
-      <Container>
-        <article className="h-screen flex justify-center items-center text-white">
-          <div className="px-1">
-            <h1 className="font-bold text-3xl mb-4">
-              Olá, me chamo Matheus Ned
-            </h1>
-            <p className="font-medium md:text-2xl">
-              Sou desenvolvedor{" "}
-              <strong className="text-blue-500">
-                Full Stack com foco em Front-end
-              </strong>
+  const text = `
+              Sou desenvolvedor Full Stack com foco em Front-end
               , criando aplicações modernas, rápidas e bem estruturadas.
               Trabalho com Next.js e TypeScript, desenvolvendo interfaces
               responsivas e performáticas utilizando Tailwind CSS e SCSS. No
@@ -22,7 +13,51 @@ export default function Intro() {
               experiência com versionamento usando Git, organização de código,
               boas práticas e arquitetura pensada para produtos SaaS. Gosto de
               transformar ideias em soluções reais, com foco em usabilidade,
-              performance e código limpo.
+              performance e código limpo.`
+
+  const [displayedText, setDisplayedText] = useState("");
+  const [showCursor, setShowCursor] = useState(true);
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setDisplayedText((prev) => prev + text.charAt(i));
+      i++;
+      if (i >= text.length) clearInterval(interval);
+    }, 30);
+
+    return () => clearInterval(interval);
+  }, [text]);
+
+  // Cursor piscando
+  useEffect(() => {
+    const cursorInterval = setInterval(() => {
+      setShowCursor((prev) => !prev);
+    }, 500);
+
+    return () => clearInterval(cursorInterval);
+  }, []);
+  return (
+    <section className="h-screen snap-start bg-blue-900">
+      <Container>
+        <article className="w-full h-screen flex justify-center items-center text-white">
+          <div className="px-1 w-full ">
+            <h1 className="font-bold text-3xl mb-4">
+              <motion.span
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  duration: 0.6,
+                  ease: "easeOut",
+                }}
+                className="inline-block"
+              >
+                Olá, me chamo Matheus Ned
+
+              </motion.span>
+            </h1>
+            <p className="font-medium md:text-2xl max-w-5xl">
+              {displayedText}
+              <span className="ml-1 text-pink-600 ">{showCursor ? "|" : " "}</span>
             </p>
           </div>
         </article>
--- src/components/projetos/projetos.tsx
+++ src/components/projetos/projetos.tsx
@@ -1,6 +1,6 @@
 export default function Projetos() {
   return (
-    <section className="h-screen snap-start flex bg-amber-900">
+    <section className="h-screen snap-start  flex bg-amber-900">
       <div className="flex-1">Infos</div>
       <div className="flex-1">Projetos</div>
     </section>
