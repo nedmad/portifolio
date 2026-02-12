b426794816f4232a6a315c9ab5edf4077085e09f
nedled <matheusmned@gmail.com>
2026-02-11 21:27:52 -0300

    Adicionado as particulas

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import Container from "../container/container";
 import { motion } from "motion/react";
+
 export default function Intro() {
   const text = `
               Sou desenvolvedor Full Stack com foco em Front-end
@@ -13,7 +14,7 @@ export default function Intro() {
               experiência com versionamento usando Git, organização de código,
               boas práticas e arquitetura pensada para produtos SaaS. Gosto de
               transformar ideias em soluções reais, com foco em usabilidade,
-              performance e código limpo.`
+              performance e código limpo.`;
 
   const [displayedText, setDisplayedText] = useState("");
   const [showCursor, setShowCursor] = useState(true);
@@ -23,7 +24,7 @@ export default function Intro() {
       setDisplayedText((prev) => prev + text.charAt(i));
       i++;
       if (i >= text.length) clearInterval(interval);
-    }, 30);
+    }, 15);
 
     return () => clearInterval(interval);
   }, [text]);
@@ -37,31 +38,34 @@ export default function Intro() {
     return () => clearInterval(cursorInterval);
   }, []);
   return (
-    <section className="h-screen snap-start bg-blue-900">
-      <Container>
-        <article className="w-full h-screen flex justify-center items-center text-white">
-          <div className="px-1 w-full ">
-            <h1 className="font-bold text-3xl mb-4">
-              <motion.span
-                initial={{ y: -20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{
-                  duration: 0.6,
-                  ease: "easeOut",
-                }}
-                className="inline-block"
-              >
-                Olá, me chamo Matheus Ned
-
-              </motion.span>
-            </h1>
-            <p className="font-medium md:text-2xl max-w-5xl">
-              {displayedText}
-              <span className="ml-1 text-pink-600 ">{showCursor ? "|" : " "}</span>
-            </p>
-          </div>
-        </article>
-      </Container>
-    </section>
+    <>
+      <section className="h-screen snap-start">
+        <Container>
+          <article className="w-full h-screen flex justify-center items-center text-white">
+            <div className="px-1 w-full min-h-80 ">
+              <h1 className="font-bold text-3xl mb-4 ">
+                <motion.span
+                  initial={{ y: -20, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{
+                    duration: 0.6,
+                    ease: "easeOut",
+                  }}
+                  className="inline-block"
+                >
+                  Olá, me chamo Matheus Ned
+                </motion.span>
+              </h1>
+              <p className="font-medium md:text-2xl max-w-5xl">
+                {displayedText}
+                <span className="ml-1 text-pink-600 ">
+                  {showCursor ? "|" : " "}
+                </span>
+              </p>
+            </div>
+          </article>
+        </Container>
+      </section>
+    </>
   );
 }
