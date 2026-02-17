9147ce8edeb55ed2ad3f70c6b309ade2f4b01f03
nedled <matheusmned@gmail.com>
2026-02-16 21:20:37 -0300

    ajustes no navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,24 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { HiOutlineMenu } from "react-icons/hi";
+import { useEffect, useRef, useState } from "react";
+import { HiOutlineMenu, HiXCircle } from "react-icons/hi";
+import style from "./header.module.css";
 export default function Header() {
   let [scrol, setScrol] = useState(0);
   const [opacity, setOpacity] = useState(false);
+  const [clickBuger, setClickBurguer] = useState(false);
+
+  function handleHiden() {
+    setClickBurguer((opac) => !opac);
+  }
+
   useEffect(() => {
     function handlescrolly() {
       if (window.scrollY > scrol) {
         setOpacity(true);
+        if (clickBuger) {
+          setClickBurguer(false);
+        }
       } else {
         setOpacity(false);
       }
@@ -20,33 +30,53 @@ export default function Header() {
   }, [scrol]);
   return (
     <header
-      className={`w-full bg-black opacity-90 flex justify-end  px-7 py-3 fixed scale-z-100 ${opacity ? "hidden" : "block"}`}
+      className={`w-full z-50  bg-black opacity-90 flex justify-end  px-7 py-3 fixed scale-z-100 ${opacity ? "hidden" : "block"}`}
     >
-      <nav className="flex gap-3 font-bold justify-center max-md:hidden opacity-100">
-        <a
-          href="#contatos"
-          className="inline-block px-3 py-2 hover:bg-purple-800 bg-purple-600 rounded transition duration-300"
+      <div className={`w-full ${clickBuger ? "h-screen" : ""} `}>
+        {!clickBuger && (
+          <div className={`w-full flex justify-end min-md:hidden bg-black`}>
+            <HiOutlineMenu
+              onClick={handleHiden}
+              className="opacity-100 hover:text-purple-800  transition duration-300 text-white"
+              size={48}
+            />
+          </div>
+        )}
+        {clickBuger && (
+          <div className={`w-full flex justify-end min-md:hidden bg-black`}>
+            <HiXCircle
+              onClick={handleHiden}
+              className="opacity-100 hover:text-purple-800  transition duration-300 text-white"
+              size={48}
+            />
+          </div>
+        )}
+
+        <nav
+          className={` font-bold opacity-100  ${clickBuger ? style.navOpen : style.nav}`}
         >
-          Contatos
-        </a>
-        <a
-          href="#projetos"
-          className="inline-block px-3 py-2 hover:bg-purple-800 bg-purple-600 rounded transition duration-300"
-        >
-          Projetos
-        </a>
-        <a
-          href="#infos"
-          className="inline-block px-3 py-2 hover:bg-purple-800 bg-purple-600 rounded transition duration-300"
-        >
-          Mais informações
-        </a>
-      </nav>
-      <div className="w-full flex justify-end min-md:hidden bg-black">
-        <HiOutlineMenu
-          className="opacity-100 hover:text-purple-800  transition duration-300 text-white"
-          size={48}
-        />
+          <a
+            href="#contatos"
+            onClick={handleHiden}
+            className="inline-block px-3 py-2 hover:bg-purple-800 bg-purple-600 rounded transition duration-300"
+          >
+            Contatos
+          </a>
+          <a
+            href="#projetos"
+            onClick={handleHiden}
+            className="inline-block px-3 py-2 hover:bg-purple-800 bg-purple-600 rounded transition duration-300"
+          >
+            Projetos
+          </a>
+          <a
+            href="#infos"
+            onClick={handleHiden}
+            className="inline-block px-3 py-2 hover:bg-purple-800 bg-purple-600 rounded transition duration-300"
+          >
+            Mais informações
+          </a>
+        </nav>
       </div>
     </header>
   );
